919080174da68649f31fce9fc49c52194d5e0fae
Uray Febri Isviladinata <ray@Urays-MacBook-Air.local>
2025-07-06 18:45:48 +0700

    fix: add visualizer

--- templates/nikahflix-react/vite.config.js
+++ templates/nikahflix-react/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
+import { visualizer } from "rollup-plugin-visualizer";
 
 const version = fs.readFileSync("./VERSION", "utf-8").trim();
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), visualizer({ filename: "stats.html", open: false })],
   define: {
     __VERSION__: JSON.stringify(version), // 👈 define global constant
   },
